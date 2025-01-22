dae4d62842ebbe93a688d3e9be8236aed833be8e
Hiba <hibakaawan98@gmail.com>
2025-01-21 23:06:01 -0600

    created route files and added to server file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import doctorRoutes from "./routes/doctors.js";
+import patientRoutes from "./routes/patients.js";
 
 
 const app = express();
